5e0454602b6742915d1e7be577a1305078a5b1a0
David Moore <mooredev@me.com>
2018-01-05 22:36:01 -0500

    Activate SFLabel Class and Enable Automatic Font Adjustment

---
+++ SFKit/Source/Environment Conforming/SFLabel.swift
@@ -0,0 +1,27 @@
+//
+//  SFLabel.swift
+//  SFKit
+//
+//  Created by David Moore on 7/01/17.
+//  Copyright © 2017 Moore Development. All rights reserved.
+//
+
+import UIKit
+
+open class SFLabel: UILabel {
+    
+    open override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        registerForAppearanceUpdates()
+    }
+    
+    open override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        registerForAppearanceUpdates()
+    }
+    
+    open override func appearanceStyleDidChange(_ newAppearanceStyle: SFAppearanceStyle) {
+        super.appearanceStyleDidChange(newAppearanceStyle)
+        adjustsFontForContentSizeCategory = true
+    }
+}
