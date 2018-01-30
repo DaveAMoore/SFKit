b49f82da45fc2ef30fc1af97962fe3b9c3b2042d
David Moore <mooredev@me.com>
2018-01-30 16:34:56 -0500

    Add Onboarding Text Field Card

---
+++ SFKit/Source/Objects/Onboarding/Cards/Text Field/SFOnboardingTextFieldCard.swift
@@ -0,0 +1,25 @@
+//
+//  SFOnboardingTextFieldCard.swift
+//  SFKit
+//
+//  Created by David Moore on 1/29/18.
+//  Copyright © 2018 Moore Development. All rights reserved.
+//
+
+final internal class SFOnboardingTextFieldCard: SFTableViewCell {
+    
+    /// Label that will present the title.
+    @IBOutlet public var titleLabel: UILabel!
+    
+    /// Text field from which input will be taken.
+    @IBOutlet public var textField: UITextField!
+    
+    public override func appearanceStyleDidChange(_ newAppearanceStyle: SFAppearanceStyle) {
+        super.appearanceStyleDidChange(newAppearanceStyle)
+        
+        // Configure the view coloring.
+        backgroundColor = SFColor.white
+        titleLabel.textColor = SFColor.black
+        textField.textColor = SFColor.black
+    }
+}
---
+++ SFKit/Source/Objects/Onboarding/Cards/Text Field/SFOnboardingTextFieldCardDescriptor.swift
@@ -0,0 +1,48 @@
+//
+//  SFOnboardingTextFieldCardDescriptor.swift
+//  SFKit
+//
+//  Created by David Moore on 1/28/18.
+//  Copyright © 2018 Moore Development. All rights reserved.
+//
+
+public class SFOnboardingTextFieldCardDescriptor: SFOnboardingCardDescriptor {
+    
+    /// Cell type that is associated with the descriptor.
+    private typealias CardType = SFOnboardingTextFieldCard
+    
+    /// Cell type that is associated with the descriptor.
+    public override var associatedCardName: String {
+        return CardType.typeName
+    }
+    
+    /// Title will be displayed alongside text field.
+    public var localizedTitle: String
+    
+    /// 
+    public var localizedPlaceholder: String?
+    
+    /// Boolean value indicating if the text field will be for secure text entry.
+    public var isSecureTextEntry: Bool
+    
+    /// Type of return key that will be presented in the cell.
+    public var returnKeyType: UIReturnKeyType
+    
+    /// Initializes a new receiver.
+    public init(localizedTitle: String, isSecureTextEntry: Bool, returnKeyType: UIReturnKeyType) {
+        self.localizedTitle = localizedTitle
+        self.isSecureTextEntry = isSecureTextEntry
+        self.returnKeyType = returnKeyType
+    }
+    
+    /// Prepares a card for presentation within a table view.
+    ///
+    /// - Parameter card: Card that must be configured for display.
+    public override func prepare(_ card: SFTableViewCell) {
+        let card = card as! CardType
+        card.titleLabel.text = localizedTitle
+        card.textField.isSecureTextEntry = isSecureTextEntry
+        card.textField.returnKeyType = returnKeyType
+        card.textField.enablesReturnKeyAutomatically = true
+    }
+}
