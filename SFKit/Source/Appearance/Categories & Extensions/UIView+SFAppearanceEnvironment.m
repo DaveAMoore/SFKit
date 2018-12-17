0426c48ad45a31b4a9f484acd6a3728879accf76
David Moore <mooredev@me.com>
2018-12-17 16:51:56 -0500

    Switch to using SFMethodSwizzler

--- SFKit/Source/Appearance/Categories & Extensions/UIView+SFAppearanceEnvironment.m
+++ SFKit/Source/Appearance/Categories & Extensions/UIView+SFAppearanceEnvironment.m
@@ -7,9 +7,9 @@
 //
 
 #import "UIView+SFAppearanceEnvironment.h"
-#import <SFKit/SFKit.h>
-#import <objc/runtime.h>
+#import <SFKit/SFMethodSwizzler.h>
 #import <SFKit/SFKit-Swift.h>
+#import <SFKit/SFKit.h>
 
 @implementation UIView (SFAppearanceEnvironment)
 @dynamic adjustsColorForAppearanceStyle;
@@ -36,9 +36,13 @@
     // Perform the method swizzling.
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        [self _extendsAppearanceStyleDidChange];
-        [self _extendsPrepareForInterfaceBuilder];
-        [self _extendsWillMoveToSuperview];
+        SFMethodSwizzler *swizzler = [[SFMethodSwizzler alloc] initWithTargetClass:self];
+        [swizzler swizzleMethodWithSelector:@selector(appearanceStyleDidChange:)
+                      forMethodWithSelector:@selector(_appearanceStyleDidChange:)];
+        [swizzler swizzleMethodWithSelector:@selector(prepareForInterfaceBuilder)
+                      forMethodWithSelector:@selector(_prepareForInterfaceBuilder)];
+        [swizzler swizzleMethodWithSelector:@selector(willMoveToSuperview:)
+                      forMethodWithSelector:@selector(_willMoveToSuperview:)];
     });
 }
 
@@ -66,81 +70,8 @@
     UIColorMetrics *previousColorMetrics = [[UIColorMetrics alloc] initForAppearanceStyle:previousAppearanceStyle];
     UIColorMetrics *currentColorMetrics = [[UIColorMetrics alloc] initForAppearance:self.appearance];
     
-    [self setBackgroundColor:[currentColorMetrics colorForColor:self.backgroundColor
-                                                     relativeTo:previousColorMetrics]];
-}
-
-/// Performs method swizzling between '-prepareForInterfaceBuilder' and '-_prepareForInterfaceBuilder'.
-+ (void)_extendsPrepareForInterfaceBuilder {
-    // Retrieve the '-prepareForInterfaceBuilder' selector and method implementation.
-    SEL prepareForInterfaceBuilderSelector = @selector(prepareForInterfaceBuilder);
-    Method prepareForInterfaceBuilderMethod = class_getInstanceMethod(self, prepareForInterfaceBuilderSelector);
-    IMP prepareForInterfaceBuilderImplementation = method_getImplementation(prepareForInterfaceBuilderMethod);
-    
-    // Retrieve the '-_prepareForInterfaceBuilder' selector and method implementation.
-    SEL _prepareForInterfaceBuilderSelector = @selector(_prepareForInterfaceBuilder);
-    Method _prepareForInterfaceBuilderMethod = class_getInstanceMethod(self, _prepareForInterfaceBuilderSelector);
-    IMP _prepareForInterfaceBuilderImplementation = method_getImplementation(_prepareForInterfaceBuilderMethod);
-    
-    // Attempt to add '-prepareForInterfaceBuilder' with the new implementation.
-    BOOL methodWasAdded = class_addMethod(self, prepareForInterfaceBuilderSelector, _prepareForInterfaceBuilderImplementation,
-                                          method_getTypeEncoding(_prepareForInterfaceBuilderMethod));
-    
-    if (methodWasAdded) {
-        // Update the selector points to the original '-prepareForInterfaceBuilder' method.
-        class_replaceMethod(self, _prepareForInterfaceBuilderSelector, prepareForInterfaceBuilderImplementation,
-                            method_getTypeEncoding(prepareForInterfaceBuilderMethod));
-    } else {
-        method_exchangeImplementations(prepareForInterfaceBuilderMethod, _prepareForInterfaceBuilderMethod);
-    }
-}
-
-+ (void)_extendsWillMoveToSuperview {
-    // Retrieve the '-prepareForInterfaceBuilder' selector and method implementation.
-    SEL willMoveToSuperviewSelector = @selector(willMoveToSuperview:);
-    Method willMoveToSuperviewMethod = class_getInstanceMethod(self, willMoveToSuperviewSelector);
-    IMP willMoveToSuperviewImplementation = method_getImplementation(willMoveToSuperviewMethod);
-    
-    // Retrieve the '-_prepareForInterfaceBuilder' selector and method implementation.
-    SEL _willMoveToSuperviewSelector = @selector(_willMoveToSuperview:);
-    Method _willMoveToSuperviewMethod = class_getInstanceMethod(self, _willMoveToSuperviewSelector);
-    IMP _willMoveToSuperviewImplementation = method_getImplementation(_willMoveToSuperviewMethod);
-    
-    // Attempt to add '-prepareForInterfaceBuilder' with the new implementation.
-    BOOL methodWasAdded = class_addMethod(self, willMoveToSuperviewSelector, _willMoveToSuperviewImplementation,
-                                          method_getTypeEncoding(_willMoveToSuperviewMethod));
-    
-    if (methodWasAdded) {
-        // Update the selector points to the original '-willMoveToSuperview' method.
-        class_replaceMethod(self, _willMoveToSuperviewSelector, willMoveToSuperviewImplementation,
-                            method_getTypeEncoding(willMoveToSuperviewMethod));
-    } else {
-        method_exchangeImplementations(willMoveToSuperviewMethod, _willMoveToSuperviewMethod);
-    }
-}
-
-+ (void)_extendsAppearanceStyleDidChange {
-    // Retrieve the '-prepareForInterfaceBuilder' selector and method implementation.
-    SEL appearanceStyleDidChangeSelector = @selector(appearanceStyleDidChange:);
-    Method appearanceStyleDidChangeMethod = class_getInstanceMethod(self, appearanceStyleDidChangeSelector);
-    IMP appearanceStyleDidChangeImplementation = method_getImplementation(appearanceStyleDidChangeMethod);
-    
-    // Retrieve the '-_prepareForInterfaceBuilder' selector and method implementation.
-    SEL _appearanceStyleDidChangeSelector = @selector(_appearanceStyleDidChange:);
-    Method _appearanceStyleDidChangeMethod = class_getInstanceMethod(self, _appearanceStyleDidChangeSelector);
-    IMP _appearanceStyleDidChangeImplementation = method_getImplementation(_appearanceStyleDidChangeMethod);
-    
-    // Attempt to add '-prepareForInterfaceBuilder' with the new implementation.
-    BOOL methodWasAdded = class_addMethod(self, appearanceStyleDidChangeSelector, _appearanceStyleDidChangeImplementation,
-                                          method_getTypeEncoding(_appearanceStyleDidChangeMethod));
-    
-    if (methodWasAdded) {
-        // Update the selector points to the original '-appearanceStyleDidChange' method.
-        class_replaceMethod(self, _appearanceStyleDidChangeSelector, appearanceStyleDidChangeImplementation,
-                            method_getTypeEncoding(appearanceStyleDidChangeMethod));
-    } else {
-        method_exchangeImplementations(appearanceStyleDidChangeMethod, _appearanceStyleDidChangeMethod);
-    }
+    UIColorMetricsHue relativeHue = [previousColorMetrics relativeHueForColor:self.backgroundColor];
+    [self setBackgroundColor:[currentColorMetrics colorForRelativeHue:relativeHue]];
 }
 
 @end
--- SFKit/Source/Appearance/Categories & Extensions/UIViewController+SFAppearanceEnvironment.m
+++ SFKit/Source/Appearance/Categories & Extensions/UIViewController+SFAppearanceEnvironment.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIViewController+SFAppearanceEnvironment.h"
+#import <SFKit/SFMethodSwizzler.h>
 #import <SFKit/SFKit.h>
 #import <objc/runtime.h>
 
@@ -35,8 +36,11 @@
     // Perform the method swizzling.
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        [self _extendsAppearanceStyleDidChange];
-        [self _extendsViewDidLoad];
+        SFMethodSwizzler *swizzler = [[SFMethodSwizzler alloc] initWithTargetClass:self];
+        [swizzler swizzleMethodWithSelector:@selector(appearanceStyleDidChange:)
+                      forMethodWithSelector:@selector(_appearanceStyleDidChange:)];
+        [swizzler swizzleMethodWithSelector:@selector(viewDidLoad)
+                      forMethodWithSelector:@selector(_viewDidLoad)];
     });
 }
 
@@ -64,55 +68,6 @@
         return UIStatusBarStyleDefault;
     } else {
         return UIStatusBarStyleLightContent;
-    }
-}
-
-/// Preforms method swizzling between '-viewDidLoad' and '-_viewDidLoad'.
-+ (void)_extendsViewDidLoad {
-    // Retrieve the '-viewDidLoad' selector and method implementation.
-    SEL viewDidLoadSelector = @selector(viewDidLoad);
-    Method viewDidLoadMethod = class_getInstanceMethod(self, viewDidLoadSelector);
-    IMP viewDidLoadImplementation = method_getImplementation(viewDidLoadMethod);
-    
-    // Retrieve the '-_viewDidLoad' selector and method implementation.
-    SEL _viewDidLoadSelector = @selector(_viewDidLoad);
-    Method _viewDidLoadMethod = class_getInstanceMethod(self, _viewDidLoadSelector);
-    IMP _viewDidLoadImplementation = method_getImplementation(_viewDidLoadMethod);
-    
-    // Attempt to add '-viewDidLoad' with the new implementation.
-    BOOL methodWasAdded = class_addMethod(self, viewDidLoadSelector, _viewDidLoadImplementation,
-                                          method_getTypeEncoding(_viewDidLoadMethod));
-    
-    if (methodWasAdded) {
-        // Update the selector points to the original '-viewDidLoad' method.
-        class_replaceMethod(self, _viewDidLoadSelector, viewDidLoadImplementation,
-                            method_getTypeEncoding(viewDidLoadMethod));
-    } else {
-        method_exchangeImplementations(viewDidLoadMethod, _viewDidLoadMethod);
-    }
-}
-
-+ (void)_extendsAppearanceStyleDidChange {
-    // Retrieve the '-prepareForInterfaceBuilder' selector and method implementation.
-    SEL appearanceStyleDidChangeSelector = @selector(appearanceStyleDidChange:);
-    Method appearanceStyleDidChangeMethod = class_getInstanceMethod(self, appearanceStyleDidChangeSelector);
-    IMP appearanceStyleDidChangeImplementation = method_getImplementation(appearanceStyleDidChangeMethod);
-    
-    // Retrieve the '-_prepareForInterfaceBuilder' selector and method implementation.
-    SEL _appearanceStyleDidChangeSelector = @selector(_appearanceStyleDidChange:);
-    Method _appearanceStyleDidChangeMethod = class_getInstanceMethod(self, _appearanceStyleDidChangeSelector);
-    IMP _appearanceStyleDidChangeImplementation = method_getImplementation(_appearanceStyleDidChangeMethod);
-    
-    // Attempt to add '-prepareForInterfaceBuilder' with the new implementation.
-    BOOL methodWasAdded = class_addMethod(self, appearanceStyleDidChangeSelector, _appearanceStyleDidChangeImplementation,
-                                          method_getTypeEncoding(_appearanceStyleDidChangeMethod));
-    
-    if (methodWasAdded) {
-        // Update the selector points to the original '-appearanceStyleDidChange' method.
-        class_replaceMethod(self, _appearanceStyleDidChangeSelector, appearanceStyleDidChangeImplementation,
-                            method_getTypeEncoding(appearanceStyleDidChangeMethod));
-    } else {
-        method_exchangeImplementations(appearanceStyleDidChangeMethod, _appearanceStyleDidChangeMethod);
     }
 }
 
